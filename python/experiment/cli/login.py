564501b173c4413b8ea42479d8e5a654a9b095d5
Alessandro Pomponio <Alessandro.Pomponio1@ibm.com>
2023-05-18 15:32:02 +0100

    feat(cli): make stp login more user friendly (#224)
    Resolves: https://github.com/st4sd/st4sd-runtime-core/issues/38
    Resolves: https://github.ibm.com/st4sd/st4sd-runtime-core/issues/213

--- python/experiment/cli/login.py
+++ python/experiment/cli/login.py
@@ -5,9 +5,11 @@
 import keyring
 import pydantic
 import requests.exceptions
+import rich.prompt
 import typer
 from pydantic import HttpUrl, ValidationError
 from rich.console import Console
+from rich.prompt import Prompt
 
 import experiment.service.db
 from experiment.cli.configuration import Configuration, Context
@@ -26,10 +28,8 @@ def login(
         context_name: str = typer.Option(default="",
                                          help="Name you want to use for this context",
                                          show_default=False),
-        access_token: str = typer.Option(...,
+        access_token: str = typer.Option("",
                                          help="Token to access this context",
-                                         prompt="Enter a valid access token for this instance",
-                                         hide_input=True,
                                          show_default=False),
         force: bool = typer.Option(default=False,
                                    help="Force overwriting of previous login details",
@@ -42,10 +42,11 @@ def login(
     Log in to an ST4SD Instance and save the details for future use.
 
     Usage:
-    stp login --access_token <token to access context>
+    stp login [--access-token <token to access context>]
     [--context-name <name to use for the context>]
     [--force] [--no-set-default] url
 
+    If the access token is not provided, the user will be prompted for it.
     In case the context-name isn't provided, it will be set to be the same as the URL.
 
     Unless --no-set-default is provided, the context you're logging in to will be set as default.
@@ -58,10 +59,37 @@ def login(
     if not url.startswith("http"):
         url = f"https://{url}"
 
+    # Rewrite the URL by stripping e.g., path and query
     try:
-        pydantic.parse_obj_as(HttpUrl, url)
+        pydantic_url = pydantic.parse_obj_as(HttpUrl, url)
+        url = f"{pydantic_url.scheme}://{pydantic_url.host}"
+        if pydantic_url.port not in ["80", "443"]:
+            url += f":{pydantic_url.port}"
     except ValidationError as e:
         stderr.print(f"{url} is not valid a valid URL: [red]{e.errors()[0].get('msg')}[/red]")
+        raise typer.Exit(code=STPExitCodes.INPUT_ERROR)
+
+    # Ask the user to provide an access token once
+    if access_token == "":
+        authorization_url = f"{url}/authorisation/token"
+        stderr.print("You need to provide an access token to connect to this instance.")
+        stderr.print(f"You can get one at {authorization_url}")
+
+        if not stdout.is_interactive:
+            stderr.print("The terminal is not interactive and we cannot proceed.")
+            raise typer.Abort()
+
+        # Give the user the possibility to open the browser
+        if rich.prompt.Confirm.ask("Open the URL in the default browser?"):
+            typer.launch(authorization_url)
+
+        access_token = Prompt.ask("Provide the access token to connect to this instance",
+                                  password=True)
+
+    # If the user still hasn't provided one, quit
+    if access_token == "":
+        stderr.print("No access token was provided.\n"
+                     "[italic]Tip: you can use the --access-token flag.[/italic]")
         raise typer.Exit(code=STPExitCodes.INPUT_ERROR)
 
     context_name = context_name.lower()
@@ -88,7 +116,7 @@ def login(
         experiment.service.db.ExperimentRestAPI(url, max_retries=2, secs_between_retries=1,
                                                 cc_auth_token=access_token)
     except experiment.service.errors.UnauthorisedRequest:
-        stderr.print("Login attempt failed. Ensure the URL and the access token are correct")
+        stderr.print("[red]Login attempt failed.[/red] Ensure the URL and the access token are correct")
         raise typer.Exit(code=STPExitCodes.UNAUTHORIZED)
     except requests.exceptions.ConnectionError:
         stderr.print(f"Ran into an error while trying to connect to {url}")
@@ -117,3 +145,4 @@ def login(
 
     config.contexts.entries[context_name] = Context(name=context_name, url=url)
     config.update()
+    stdout.print("[green]Success![/green]")
